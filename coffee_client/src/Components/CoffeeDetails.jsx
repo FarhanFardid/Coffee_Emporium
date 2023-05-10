10149390bbee3e7ebf48ad4eda308883392df919
FarhanFardid <farhanfardid009@gmail.com>
2023-05-10 23:15:43 +0600

    coffee details card completed

--- coffee_client/src/Components/CoffeeDetails.jsx
+++ coffee_client/src/Components/CoffeeDetails.jsx
@@ -11,10 +11,12 @@ const CoffeeDetails = () => {
    <div className="ms-32" > <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={photo_url} alt="coffee photo"/></div>
     <div className="flex flex-col text-gray-900 justify-between p-4 leading-normal me-36 ">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+        <p className="mb-3 font-medium "><span className="font-bold">Product Id:</span> {_id} </p>
         <p className="mb-3 font-medium "><span className="font-bold">Supplier:</span> {supplier} </p>
         <p className="mb-3 font-medium "><span className="font-bold">Category:</span> {category} </p>
         <p className="mb-3 font-medium "><span className="font-bold">Details:</span> {details} </p>
         <p className="mb-3 font-medium "><span className="font-bold">Price:</span> {price} </p>
+       
     </div>
 </div>
 
